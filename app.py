e28cb640595f66726d0aab825c4d18b163baf659
Oscar Caicedo <oscarccd17@gmail.com>
2022-10-10 23:06:13 -0500

    .click w/out actiondrivers, added a wait time before quitting

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
+import time
+# from selenium.webdriver.common.action_chains import ActionChains
 
 
 driver = webdriver.Chrome()
-
 driver.get("https://oscardevelops.com/")
+print(driver.title)
 
-element = driver.find_element(
-    By.XPATH, '//*[@id="menu-item-121"]/a')
+element = driver.find_element(By.CSS_SELECTOR, "#ast-desktop-header > div > div > div > div > div.site-header-primary-section-right.site-header-section.ast-flex.ast-grid-right-section > div.ast-builder-layout-element.ast-flex.site-header-focus-item.ast-header-button-1 > div > a.ast-custom-button-link")
+element.click()
+# element.send_keys(Keys.RETURN)
 
 
-actions = ActionChains(driver)
-actions.click(element)
-actions.perform()
+time.sleep(5)
+driver.quit()
+
+
+
+
+
+
+
+
+
+# element = driver.find_element(
+#     By.XPATH, '//*[@id="menu-item-121"]/a')
+
+# actions = ActionChains(driver)
+# actions.click(element)
+# actions.perform()
