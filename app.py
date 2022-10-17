54d1204b50bca2905583a1f4f1358ad5bd91c246
Oscar Caicedo <oscarccd17@gmail.com>
2022-10-16 20:32:44 -0500

    working on scraping page content

--- app.py
+++ app.py
@@ -7,43 +7,51 @@ import time
 
 driver = webdriver.Chrome()
 driver.get("https://oscardevelops.com/")
-print(driver.title)
+# print(driver.title)
 
-element = driver.find_element(By.CSS_SELECTOR, "#ast-desktop-header > div > div > div > div > div.site-header-primary-section-right.site-header-section.ast-flex.ast-grid-right-section > div.ast-builder-layout-element.ast-flex.site-header-focus-item.ast-header-button-1 > div > a.ast-custom-button-link")
-element.click()
-# element.send_keys(Keys.RETURN)
+# element = driver.find_element(By.CSS_SELECTOR, "#ast-desktop-header > div > div > div > div > div.site-header-primary-section-right.site-header-section.ast-flex.ast-grid-right-section > div.ast-builder-layout-element.ast-flex.site-header-focus-item.ast-header-button-1 > div > a.ast-custom-button-link")
+# element.click()
+# # element.send_keys(Keys.RETURN)
 
-form1 = driver.find_element(By.ID, 'wpforms-8-field_4')
-form1.clear()
-form1.send_keys('Michael Scott')
+# form1 = driver.find_element(By.ID, 'wpforms-8-field_4')
+# form1.clear()
+# form1.send_keys('Michael Scott')
 
-form2 = driver.find_element(By.ID, 'wpforms-8-field_1')
-form2.clear()
-form2.send_keys('Mscott@papercompany.com')
+# form2 = driver.find_element(By.ID, 'wpforms-8-field_1')
+# form2.clear()
+# form2.send_keys('Mscott@papercompany.com')
 
-form3 = driver.find_element(By.ID, 'wpforms-8-field_3')
-form3.clear()
-form3.send_keys('Need a Website')
+# form3 = driver.find_element(By.ID, 'wpforms-8-field_3')
+# form3.clear()
+# form3.send_keys('Need a Website')
 
-form4 = driver.find_element(By.ID, 'wpforms-8-field_2')
-form4.clear()
-form4.send_keys('Hi, I need help with a website. I could use a quote for the work.')
+# form4 = driver.find_element(By.ID, 'wpforms-8-field_2')
+# form4.clear()
+# form4.send_keys(
+#     'Hi, I need help with a website. I could use a quote for the work.')
 
-btn = driver.find_element(By.ID, 'wpforms-submit-8')
-btn.click()
+# btn = driver.find_element(By.ID, 'wpforms-submit-8')
+# btn.click()
 
-driver.back()
+client_services = driver.find_element(
+    By.CSS_SELECTOR, '#menu-item-120 > a').click()
+
+client_title = driver.find_element(
+    By.XPATH, '// *[@id="post-112"]/div/div/section[2]/div/div[1]/div/div[2]/div/div/div/h4')
+
+client_paragraph = driver.find_element(
+    By.XPATH, '//*[@id="post-112"]/div/div/section[2]/div/div[1]/div/div[2]/div/div/div/p')
+
+split = client_paragraph.text.strip(" ")
+
+# print(client_title.text)
+print(len(split))
 
 
-time.sleep(5)
+
+# driver.back()
+# time.sleep(5)
 driver.quit()
-
-
-
-
-
-
-
 
 
 # element = driver.find_element(
